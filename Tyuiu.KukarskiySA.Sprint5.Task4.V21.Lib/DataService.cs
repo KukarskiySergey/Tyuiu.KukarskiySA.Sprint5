5d793c2a101f9d720f9b22264a73dce6f58e418e
KukarskiySergey <Tyuiu.KukarskiySergey@outlook.com>
2024-12-22 19:20:11 +0500

    Task4

---
+++ Tyuiu.KukarskiySA.Sprint5.Task4.V21.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KukarskiySA.Sprint5.Task4.V21.Lib
+{
+    public class DataService : ISprint5Task4V21
+    {
+        public double LoadFromDataFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"Файл не найден: {path}");
+            }
+
+            try
+            {
+                // Чтение содержимого файла
+                string content = File.ReadAllText(path).Trim();
+                if (!double.TryParse(content, out double x))
+                {
+                    throw new FormatException("Содержимое файла невозможно преобразовать в число.");
+                }
+
+                // Вычисление по формуле
+                double y = Math.Pow(x, 3) * Math.Cos(x) + 2 * x;
+
+                // Округление до трех знаков после запятой
+                return Math.Round(y, 3);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка при обработке файла или вычислении.", ex);
+            }
+        }
+    }
+}
---
+++ Tyuiu.KukarskiySA.Sprint5.Task4.V21/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KukarskiySA.Sprint5.Task4.V21.Lib;
+
+const string filePath = @"C:\DataSprint5\InPutDataFileTask5V21.txt";
+var dataService = new DataService();
+
+try
+{
+    // Вызываем метод и получаем результат
+    double result = dataService.LoadFromDataFile(filePath);
+    Console.WriteLine($"Результат вычисления: {result}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+}
